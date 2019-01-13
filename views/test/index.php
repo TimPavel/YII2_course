49ad4916e5bc1a4e99613af485b827e3a42e9c0f
TimPavel <timpn@yandex.ru>
2019-01-13 15:25:47 +0300

    for homework-1

---
+++ views/test/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * TestController's view.
+ * @var $product \app\models\Product
+ * @param DetailView yii\widgets\DetailView
+ */
+
+use yii\widgets\DetailView;
+?>
+
+<h1><?='Hello!'?></h1>
+
+<?= DetailView::widget([
+	'model' => $product,
+	'attributes' => [
+		'id',
+		'name',
+		'category',
+		'price',
+	],
+]);
+?>
+
+
+
+
