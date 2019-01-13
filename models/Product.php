d4d22e8b587b7965b940fd3843c507935dca3f38
TimPavel <timpn@yandex.ru>
2019-01-13 15:25:37 +0300

    for homework-1

---
+++ models/Product.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TPN
+ * Date: 12.01.2019
+ * Time: 20:02
+ */
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class Product
+{
+	public $id;
+	public $name;
+	public $category;
+	public $price;
+}
