6e85df150c1721aba1a5f83f255219ce6ccec80f
Pavel <timpn@yandex.ru>
2019-04-18 15:57:31 +0300

    disable test

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -14,12 +14,12 @@ use yii\web\Controller;
 class TestController extends Controller
 {
 	public function actionIndex() {
-
+        return $this->render('index');
 
 	}
 
 	public function actionShow() {
-
+        echo "Hello! This testController' show page";
 
 	}
 
