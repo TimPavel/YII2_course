0ccfec1ff927e23251ddc961be12181e68603d75
Pavel <timpn@yandex.ru>
2019-04-18 15:35:34 +0300

    disable Gii

--- config/web.php
+++ config/web.php
@@ -72,12 +72,14 @@ if (YII_ENV_DEV) {
         'allowedIPs' => ['*'],
     ];
 */
+   /*
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         'allowedIPs' => ['*'],
     ];
+   */
 }
 
 return $config;
